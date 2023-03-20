6f2492339b3a6cedeaacf7050a31876f948dae1c
jeffreywastaken <jeffreyixora@gmail.com>
2023-03-20 11:03:40 +0400

    second lambda working

--- lambdafunction2.py
+++ lambdafunction2.py
@@ -1,34 +1,58 @@
-import json
 import boto3
+import json
 
 
 topic_arn = 'arn:aws:sns:us-east-1:232224276285:my-topic'
+email = 'jeffreyisora@gmail.com'
 
 def lambda_handler(event, context):
-    # Extract message from the event and get the image name
-    message = json.loads(event['Records'][0]['body'])
-    image_name = message['image_name']
-    
-    # Check if the label pedestrian is in the detected labels
-    labels = message['labels']
-    if 'Pedestrian' in labels:
-        # Get the subscribed email address from the environment variables
-        email = 'jeffreyisora@gmail.com'
+    try:
+        message = json.loads(event['Records'][0]['Sns']['Message'])
+        labels = message['labels']
+        pedestrian_detected = False
+        image_names = []
+        for label in labels:
+            if label['Name'].lower() == 'pedestrian':
+                pedestrian_detected = True
+                image_names.append(message['s3_key'])
+                break
+        if pedestrian_detected:
+            sns = boto3.client('sns')
+            response = sns.publish(
+                TopicArn= topic_arn,
+                Message='Pedestrian detected in images: {}'.format(image_names)
+            )
+            print(response)
+    except Exception as e:
+        print(e)
+        raise e
+
+    # if 'Pedestrian' in labels:
+    #     # Get the subscribed email address from the environment variables
+    #     email = 'jeffreyisora@gmail.com'
         
-        # Create the SNS client
-        sns = boto3.client('sns')
+    #     # Create the SNS client
+    #     sns = boto3.client('sns')
         
-        # Send the notification email to the subscribed email address
-        subject = 'Pedestrian detected in image {}'.format(image_name)
-        message = 'Pedestrian was detected in image {}. Labels: {}'.format(image_name, labels)
-        response = sns.publish(
-            TopicArn=topic_arn,
-            Message=message,
-            Subject=subject
-        )
+    #     # Send the notification email to the subscribed email address
+    #     subject = 'Pedestrian detected in image {}'.format(image_name)
+    #     message = 'Pedestrian was detected in image {}. Labels: {}'.format(image_name, labels)
+    #     response = sns.publish(
+    #         TopicArn=topic_arn,
+    #         Message=message,
+    #         Subject=subject
+    #     )
         
-        # Print the response from the publish method to the CloudWatch logs
-        print('Notification email sent to {} with response: {}'.format(email, response))
+    #     # Print the response from the publish method to the CloudWatch logs
+    #     print('Notification email sent to {} with response: {}'.format(email, response))
         
-    else:
-        print('No pedestrian detected in image {}. Labels: {}'.format(image_name, labels))
+    # else:
+    #     print('No pedestrian detected in image {}. Labels: {}'.format(image_name, labels))
+
+
+    # response = sns.publish(
+    #         TopicArn=topic_arn,
+    #         Message=json.dumps({'default': json.dumps(message)}),
+    #         MessageStructure='json',
+    #         Subject='Pedestrian Detected',
+    #         TargetArn='arn:aws:sns:us-east-1:123456789012:email-subscription:jeffreyisora@gmail.com'
